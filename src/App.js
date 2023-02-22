9f7b2f66c62024c59fdedd51a41dfc000d863a0d
jahanvi jnext <jahanvi.jnext@gmail.com>
2023-02-22 17:59:28 +0530

    ForMilk Validation Practice

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Formilkdemo from "./Components/Formilkdemo";
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <Formilkdemo/><br/>
+    </div>
+  );
+}
+
+export default App;
